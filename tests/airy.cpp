691d259101b17ece7670ce4a051898e987aecaa0
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-08 19:01:26 +0800

    test: bessel/Hyp2f1, Errors

--- tests/airy.cpp
+++ tests/airy.cpp
@@ -76,3 +76,17 @@ TEST(Airy, Branches) {
     XTEST_ISAPPROX_F64(bi);
     XTEST_ISAPPROX_F64(bip);
 }
+
+TEST(Hyp2f1, Errors) {
+    double a, b, c, x, y, y_ref;
+
+    // c is a negative integer
+    // hypdiv:
+    EXPECT_GT(hyp2f1(2.0, 3.0, -4.0, 5.0), 1e308);
+    // ax > 1.0
+    EXPECT_GT(hyp2f1(2.0, 3.0, 4.0, 2.0), 1e308);
+}
+
+TEST(Hyp2f1, Branches) {
+    double a, b, c, x, y, y_ref;
+}
