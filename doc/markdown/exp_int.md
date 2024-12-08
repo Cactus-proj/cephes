766f2872ddf21e78801df77764c1c8213f19f758
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-08 17:14:01 +0800

    doc: add cephes\exp_int.h

---
+++ doc/markdown/exp_int.md
@@ -0,0 +1,8 @@
+# cephes/Exponential integral
+
+* **expn**, [Exponential integral En](doubldoc.md#expn)
+
+## Sine and Cosine integral
+
+* **sici**, [Sine and cosine integrals](doubldoc.md#sici)
+* **shichi**, [Hyperbolic sine and cosine integrals](doubldoc.md#shichi)
---
+++ include/cephes/exp_int.h
@@ -0,0 +1,25 @@
+#ifndef CEPHES_EXP_INT_H
+/** Cephes double precision special functions suite
+ * 
+ * cephes/Exponential integral
+ */
+#define CEPHES_EXP_INT_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/** Exponential integral */
+/* misc/expn.c */
+double expn(int n, double x);
+
+/** Sine and Cosine integral */
+/* misc/sici.c */
+int sici(double x, double *si, double *ci);
+/** misc/shichi.c */
+int shichi(double x, double *si, double *ci);
+
+#if defined(__cplusplus)
+}
+#endif
+#endif // CEPHES_EXP_INT_H
