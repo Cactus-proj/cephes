db507554f8bb116716a633bb2b86fe997c88ae97
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-08 22:52:59 +0800

    test: bessel: init BesselI1Exp, BesselIv

---
+++ tests/bessel/i1e.cpp
@@ -0,0 +1,23 @@
+#include <xtest.hpp>
+#include <cephes/bessel.h>
+
+/**
+    Table[NumberForm[Exp[-Abs[x]]*BesselI[1, x], 16],
+        {x, {0.0, -10., 1.0, 8.0, 9.0, 10.0, 100.}}]
+ */
+TEST(BesselI1Exp, Branches) {
+    EXPECT_REL_NEAR_F64(cephes::i1e(0.0), 0.0);
+
+    // x < 0
+    EXPECT_REL_NEAR_F64(cephes::i1e(-0.0), 0.0);
+    // EXPECT_REL_NEAR_F64(cephes::i1e(-10.0), );
+
+    // // x <= 8.0
+    // EXPECT_REL_NEAR_F64(cephes::i1e(1.0), );
+    // EXPECT_REL_NEAR_F64(cephes::i1e(8.0), );
+
+    // // x > 8.0
+    // EXPECT_REL_NEAR_F64(cephes::i1e(9.0),  );
+    // EXPECT_REL_NEAR_F64(cephes::i1e(10.0), );
+    // EXPECT_REL_NEAR_F64(cephes::i1e(100.0), );
+}
---
+++ tests/bessel/iv.cpp
@@ -0,0 +1,26 @@
+#include <xtest.hpp>
+#include <cephes/bessel.h>
+
+/**
+    Table[NumberForm[Exp[-Abs[x]]*BesselI[nv, x], 16],
+        {nv, {0, 1}},
+        {x, {0.0, 1.0, 8.0, 9.0, 1.0, 100.}}]
+ */
+TEST(BesselIv, Branches) {
+    EXPECT_REL_NEAR_F64(cephes::iv(0, 0.0), 1.0);
+    EXPECT_REL_NEAR_F64(cephes::iv(1, 0.0), 0.0);
+    EXPECT_REL_NEAR_F64(cephes::iv(-1, 0.0), 0.0);
+
+    // x < 0
+    EXPECT_REL_NEAR_F64(cephes::iv(0, -0.0), 1.0);
+    // EXPECT_REL_NEAR_F64(cephes::iv(-10.0), );
+
+    // // x <= 8.0
+    // EXPECT_REL_NEAR_F64(cephes::iv(1.0), );
+    // EXPECT_REL_NEAR_F64(cephes::iv(8.0), );
+
+    // // x > 8.0
+    // EXPECT_REL_NEAR_F64(cephes::iv(9.0),  );
+    // EXPECT_REL_NEAR_F64(cephes::iv(10.0), );
+    // EXPECT_REL_NEAR_F64(cephes::iv(100.0), );
+}
