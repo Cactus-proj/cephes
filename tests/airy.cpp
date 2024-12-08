1b741ba354712a684bebced0ca72ec7ccc03a755
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-08 16:21:06 +0800

    ci: init code Coverage

---
+++ tests/airy.cpp
@@ -0,0 +1,18 @@
+#include <limits>
+#include <complex>
+#include <gtest/gtest.h>
+#include <cephes/double.h>
+
+TEST(Airy, BasicAssertions) {
+  const double nan64 = std::numeric_limits<double>::quiet_NaN();
+  int ret;
+  double x, ai, aip, bi, bip;
+
+  x = 26.0;
+  ret = airy(x, &ai, &aip, &bi, &bip);
+  EXPECT_EQ(ret, -1);
+  EXPECT_EQ(ai, 0.0);
+  EXPECT_EQ(aip, 0.0);
+  EXPECT_TRUE(bi > 1e308);
+  EXPECT_TRUE(bip > 1e308);
+}
