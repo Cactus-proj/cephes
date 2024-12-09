a5c7ac7da5275beb8f131139007bec3ce1ade4e0
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-10 00:07:45 +0800

    test.gamma: add BasicAssertions for gamma

--- tests/gamma/gamma.cpp
+++ tests/gamma/gamma.cpp
@@ -4,6 +4,9 @@
 TEST(Gamma, BasicAssertions)
 {
     EXPECT_TRUE(std::isnan(cephes::gamma(xtest::NaN64)));
+    EXPECT_TRUE(std::isnan(cephes::gamma(-xtest::NaN64)));
+    EXPECT_TRUE(std::isinf(cephes::gamma(xtest::Inf64)));
+    EXPECT_TRUE(std::isnan(cephes::gamma(-xtest::Inf64))); // gamma(-Inf) == NaN
     EXPECT_TRUE(std::isnan(cephes::gamma(0.0)));
 
     EXPECT_REL_NEAR_F64(cephes::gamma(1.0), 1.0);
