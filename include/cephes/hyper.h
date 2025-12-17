b1d5c29253e8b32ccf50217ebe8dc0dea1700dd9
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-17 16:52:01 +0800

    docs(hyper): update doc

--- include/cephes/hyper.h
+++ include/cephes/hyper.h
@@ -1,7 +1,6 @@
 #ifndef CEPHES_HYPER_H
-/** Cephes double precision special functions suite
+/** Cephes: Hypergeometric Functions
  *
- * cephes/bessel
  */
 #define CEPHES_HYPER_H
 
@@ -10,8 +9,25 @@ namespace cephes {
 extern "C" {
 #endif
 
-/** Hypergeometric Functions */
+/**
+ * @brief Computes the gauss hypergeometric function `2F1(a,b;c;x)`
+ *
+ * @param a
+ * @param b
+ * @param c
+ * @param x, |x| > 1 is rejected.
+ * @return double
+ */
 double hyp2f1(double a, double b, double c, double x);
+
+/**
+ * @brief Computes the confluent hypergeometric function `1F1(a,b;x)` (`M(a,b,x)`)
+ *
+ * @param a
+ * @param b, b must not be a negative integer or zero, unless a is an integer with `0 >= a > b`
+ * @param x
+ * @return double
+ */
 double hyperg(double a, double b, double x);
 
 #if defined(__cplusplus)
