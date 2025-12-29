8dc02ca3387d8e573af6bb4163eca96b45924873
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-29 22:00:37 +0800

    test(bessel/j1): add rand tests

--- tests/bessel/j1.cpp
+++ tests/bessel/j1.cpp
@@ -1,10 +1,41 @@
 #include <cephes/bessel.h>
 #include <xtest.hpp>
+#include <random>
 
-TEST(BesselJ1, BasicAssertions) {
-    EXPECT_EQ(cephes::j1(0.0), 0.0);
-    EXPECT_EQ(cephes::j1(-0.0), 0.0);
+TEST(BesselJ1, BrokenTests) {
+    GTEST_SKIP() << "Broken Tests";
+    /* Wolframe 
+        BesselJ[1, Infinity] == 0
+        BesselJ[1, -Infinity] == 0
+    */
+    EXPECT_EQ(cephes::j1(xtest::Inf64), 0.0);   // NaN
+    EXPECT_EQ(cephes::j1(-xtest::Inf64), 0.0);  // NaN
 }
+
+TEST(BesselJ1, SpecialValues) {
+    EXPECT_TRUE(std::isnan(cephes::j1(xtest::NaN64)));
+    EXPECT_TRUE(std::isnan(cephes::j1(-xtest::NaN64)));
+    EXPECT_EQ(cephes::j1(0.0), 0.0);
+    EXPECT_EQ(cephes::j1(-0.0), -0.0);
+}
+
+TEST(BesselJ1, OddPropertyRandom) {
+    // Odd function: j1(-x) = -j1(x)
+    EXPECT_TRUE(std::isinf(cephes::j1(-10.0)));
+    GTEST_SKIP() << "Broken Tests: j1(-x), x<0 is Inf";
+
+    std::mt19937 rng(0xC0FFEEu);
+    std::uniform_real_distribution<double> dist(0.0, 10.0);
+    for (int i = 0; i < 128; ++i) {
+        double x = dist(rng);
+        double y1, y2;
+        y1 = cephes::j1(x);
+        y2 = cephes::j1(-x);
+        SCOPED_TRACE(testing::Message() << "x=" << x << " j1(x)=" << y1 << " j1(-x)=" << y2);
+        EXPECT_REL_NEAR_F64(y2, (-y1));
+    }
+}
+
 TEST(BesselJ1, Branches) {
     double x, y, y_ref;
 
