b252d9c46901e5ac1f1d510539dc1270bacc55c4
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-29 23:30:37 +0800

    docs(bessel): add doc for Airy, BesselJ

--- include/cephes/bessel.h
+++ include/cephes/bessel.h
@@ -1,7 +1,13 @@
 #ifndef CEPHES_BESSEL_H
 /** Cephes double precision special functions suite
  *
- * cephes/bessel
+ * src: `cephes/bessel`
+ *
+ * - Airy Functions:  `airy`
+ * - Bessel functions:  `j0`, `j1`, `jn`, `jv`
+ * - Modified Bessel functions:  `i0`, `i0e`, `i1`, `i1e`, `iv`
+ * - Modified Bessel functions - second kind: `y0`, `y1`, `yn`, `yv`
+ * - Modified Bessel functions - third kind:  `k0`, `k0e`, `k1`, `k1e`, `kn`
  */
 #define CEPHES_BESSEL_H
 
@@ -10,14 +16,51 @@ namespace cephes {
 extern "C" {
 #endif
 
-/** Airy Functions */
-int airy(double x, double *ai, double *aip, double *bi, double *bip);
+/* Airy Functions */
+/**
+ * @brief Airy functions
+ * 
+ * @param[in] x 
+ * @param[out] ai   `Ai(x)`
+ * @param[out] aip  `Ai'(x)`
+ * @param[out] bi   `Bi(x)`
+ * @param[out] bip  `Bi'(x)`
+ * @return int  0=success; others=error
+ */
+int airy(const double x, double *ai, double *aip, double *bi, double *bip);
 
-/** Bessel functions */
+/* Bessel functions */
+/**
+ * @brief Bessel function of the first kind of order zero.
+ * 
+ * @param[in] x 
+ * @return double `J_0(x)`
+ */
 double j0(double x);
+/**
+ * @brief Bessel function of the first kind of order one.
+ * 
+ * @param[in] x
+ * @return double `J_1(x)`
+ * @note `J_1(x)` is `Inf` for `x <= 0.0` (xsf fixed this).
+ */
 double j1(double x);
+/**
+ * @brief Bessel function of the first kind of integer order `n`.
+ * 
+ * @param[in] n integer order
+ * @param[in] x 
+ * @return double `J_n(x)`
+ */
 double jn(int n, double x);
-double jv(double n, double x);
+/**
+ * @brief Bessel function of the first kind of real order `v`.
+ * 
+ * @param[in] v real order
+ * @param[in] x Negative `x` is allowed if `v` is an integer.
+ * @return double `J_v(x)`
+ */ 
+double jv(double v, double x);
 
 /** Modified Bessel functions */
 double i0(double x);
