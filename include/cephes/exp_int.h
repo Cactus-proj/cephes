4314601b22b8114805048ff3bb51fa2409f24092
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-30 18:13:43 +0800

    docs(exp_int): add docs

--- include/cephes/exp_int.h
+++ include/cephes/exp_int.h
@@ -1,7 +1,12 @@
 #ifndef CEPHES_EXP_INT_H
-/** Cephes double precision special functions suite
+/** @file exp_int.h
+ * Cephes double precision special functions suite
  *
- * cephes/Exponential integral
+ * Source: `cephes/misc`
+ * Provides:
+ * - Exponential integral:  expn
+ * - Sine and Cosine integral:  sici
+ * - Hyperbolic Sine and Cosine integral:  shichi
  */
 #define CEPHES_EXP_INT_H
 
@@ -10,14 +15,36 @@ namespace cephes {
 extern "C" {
 #endif
 
-/** Exponential integral */
-/* misc/expn.c */
+/* Exponential integral */
+/**
+ * @brief Exponential integral
+ * 
+ * @param n non-negative
+ * @param x non-negative
+ * @return double `E_n(x)`
+ */
 double expn(int n, double x);
 
-/** Sine and Cosine integral */
-/* misc/sici.c */
+/* Sine and Cosine integral */
+/**
+ * @brief Sine and Cosine integral
+ * 
+ * @param[in] x 
+ * @param[out] si Sine integral `Si(x)`
+ * @param[out] ci Cosine integral `Ci(x)`
+ * @return int 0 for success
+ */
 int sici(double x, double *si, double *ci);
-/** misc/shichi.c */
+
+/* Hyperbolic Sine and Cosine integral */
+/**
+ * @brief Hyperbolic Sine and Cosine integral
+ * 
+ * @param[in] x 
+ * @param[out] si Hyperbolic sine integral `Shi(x)`
+ * @param[out] ci Hyperbolic cosine integral `Chi(x)`
+ * @return int 0 for success
+ */
 int shichi(double x, double *si, double *ci);
 
 #if defined(__cplusplus)
