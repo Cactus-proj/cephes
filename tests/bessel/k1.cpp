81b83ba17cff34b6638721bfc52c1682fe453c82
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-30 13:31:31 +0800

    test(bessel/k1[e]): add more tests

--- tests/bessel/k1.cpp
+++ tests/bessel/k1.cpp
@@ -2,21 +2,24 @@
 #include <xtest.hpp>
 
 /*
-    Table[NumberForm[BesselK[1, x], 16],
-        {x, {0.0, -10., 1.0, 8.0, 9.0, 10.0, 100.}}]
+In[]:= Table[NumberForm[BesselK[1,x],16],
+            {x,{-2.7,-1.,0.,1.,2.,3.14}}]
+Out[]= {
+    -0.05773839895652593-9.47538177127153 I,
+    -0.6019072301972347-1.775499689212181 I,
+    ComplexInfinity,
+    0.6019072301972347,0.1398658818165224,0.03397775659595887}
 */
 TEST(BesselK1, Branches) {
-    EXPECT_GT(cephes::k1(0.0), 1.0e308); // +Inf
+    // x <= 0.0
+    EXPECT_GT(cephes::k1(-2.7), 1.0e308); // DOMAIN error
+    EXPECT_GT(cephes::k1(-1.0), 1.0e308); // DOMAIN error
+    EXPECT_GT(cephes::k1(0.0), 1.0e308);  // DOMAIN error
 
-    // x < 0
-    // EXPECT_REL_NEAR_F64(cephes::k1(-10.0), );
+    // x <= 2.0
+    EXPECT_REL_NEAR_F64(cephes::k1(1.0), 0.6019072301972347);
+    EXPECT_REL_NEAR_F64(cephes::k1(2.0), 0.1398658818165224);
 
-    // // x <= 8.0
-    // EXPECT_REL_NEAR_F64(cephes::k1(1.0), );
-    // EXPECT_REL_NEAR_F64(cephes::k1(8.0), );
-
-    // // x > 8.0
-    // EXPECT_REL_NEAR_F64(cephes::k1(9.0),  );
-    // EXPECT_REL_NEAR_F64(cephes::k1(10.0), );
-    // EXPECT_REL_NEAR_F64(cephes::k1(100.0), );
+    // x > 2.0
+    EXPECT_REL_NEAR_F64(cephes::k1(3.14), 0.03397775659595887);
 }
--- tests/bessel/k1e.cpp
+++ tests/bessel/k1e.cpp
@@ -2,21 +2,24 @@
 #include <xtest.hpp>
 
 /*
-    Table[NumberForm[Exp[-Abs[x]]*BesselK[1, x], 16],
-        {x, {0.0, -10., 1.0, 8.0, 9.0, 10.0, 100.}}]
+In[]:= Table[NumberForm[Exp[Abs[x]]*BesselK[1,x],16],
+            {x,{-2.7,-1.,0.,1.,2.,3.14}}]
+Out[]= {
+    -0.859131886696784-140.9911387472708 I,
+    -1.636153486263258-4.826308541620153 I,
+    ComplexInfinity,
+    1.636153486263258,1.033476847068689,0.7850175645511034}
 */
 TEST(BesselK1Exp, Branches) {
-    EXPECT_GT(cephes::k1e(0.0), 1.0e308); // +Inf
+    // x <= 0.0
+    EXPECT_GT(cephes::k1e(-2.7), 1.0e308); // DOMAIN error
+    EXPECT_GT(cephes::k1e(-1.0), 1.0e308); // DOMAIN error
+    EXPECT_GT(cephes::k1e(0.0), 1.0e308);  // DOMAIN error
 
-    // x < 0
-    // EXPECT_REL_NEAR_F64(cephes::k1e(-10.0), );
+    // x <= 2.0
+    EXPECT_REL_NEAR_F64(cephes::k1e(1.0), 1.636153486263258);
+    EXPECT_REL_NEAR_F64(cephes::k1e(2.0), 1.033476847068689);
 
-    // // x <= 8.0
-    // EXPECT_REL_NEAR_F64(cephes::k1e(1.0), );
-    // EXPECT_REL_NEAR_F64(cephes::k1e(8.0), );
-
-    // // x > 8.0
-    // EXPECT_REL_NEAR_F64(cephes::k1e(9.0),  );
-    // EXPECT_REL_NEAR_F64(cephes::k1e(10.0), );
-    // EXPECT_REL_NEAR_F64(cephes::k1e(100.0), );
+    // x > 2.0
+    EXPECT_REL_NEAR_F64(cephes::k1e(3.14), 0.7850175645511034);
 }
