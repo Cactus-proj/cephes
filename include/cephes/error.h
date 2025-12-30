316e6e2dc79512b1665fce7b0e0cea66129afcdc
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-30 18:08:32 +0800

    docs(error): add docs

--- include/cephes/error.h
+++ include/cephes/error.h
@@ -1,7 +1,12 @@
 #ifndef CEPHES_ERROR_H
-/** Cephes double precision special functions suite
+/** @file error.h
+ * Cephes double precision special functions suite
  *
- * cephes/bessel
+ * Source: `cephes/cprob, cephes/misc`
+ * Provides:
+ * - Error functions:  erf, erfc
+ * - Dawson integral:  dawsn
+ * - Fresnel integrals:  fresnl
  */
 #define CEPHES_ERROR_H
 
@@ -10,15 +15,42 @@ namespace cephes {
 extern "C" {
 #endif
 
-/** Error Functions */
-/* cephes/ndtr.c */
+/* Error Functions */
+/* cprob/ndtr.c */
+/**
+ * @brief Error function
+ * 
+ * @param x 
+ * @return double `erf(x)`
+ */
 double erf(double x);
+/**
+ * @brief Complementary error function
+ * 
+ * @param a 
+ * @return double `erfc(a)`
+ */
 double erfc(double a);
 
-/* cephes/dawsn.c */
-double dawsn(double xx);
-/* cephes/fresnl.c */
-int fresnl(double xxa, double *ssa, double *cca);
+/* misc/dawsn.c */
+/**
+ * @brief Dawson's Integral
+ * 
+ * @param x 
+ * @return double `dawsn(x)`
+ */
+double dawsn(double x);
+
+/* misc/fresnl.c */
+/**
+ * @brief Fresnel integrals
+ * 
+ * @param[in] x 
+ * @param[out] ss Fresnel integrals `S(x)`
+ * @param[out] cc Fresnel integrals `C(x)`
+ * @return int 0 for success
+ */
+int fresnl(double x, double *ss, double *cc);
 
 #if defined(__cplusplus)
 } // extern "C"
