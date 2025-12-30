351d93964e3248a033ec1d8b777e23ef9659aa33
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-30 17:59:03 +0800

    docs(elliptic): add doc for Elliptic Functions

--- include/cephes/elliptic.h
+++ include/cephes/elliptic.h
@@ -1,7 +1,11 @@
 #ifndef CEPHES_ELLIPTIC_H
-/** Cephes double precision special functions suite
+/** @file elliptic.h
+ * Cephes double-precision elliptic integrals and Jacobian elliptic functions (interfaces)
  *
- * cephes/bessel
+ * Source: `cephes/ellf`
+ * Provides:
+ * - Elliptic integrals:  ellpk, ellik, ellpe, ellie
+ * - Jacobian elliptic functions:  ellpj
  */
 #define CEPHES_ELLIPTIC_H
 
@@ -10,18 +14,54 @@ namespace cephes {
 extern "C" {
 #endif
 
-/** Elliptic Functions */
-/* ellf/ellpk.c */
-double ellpk(double x);
-/* ellf/ellik.c */
+/* Elliptic Functions */
+/**
+ * @brief Complete elliptic integral of the first kind
+ *
+ * @param m1 parameter `m1 = 1 - m`, in `[0, 1]`
+ * @return double `K(m)`.
+ *  Returns 0.0 if m1 is outside [0,1]; 
+ *  returns MAXNUM at the singularity m1 == 0
+ */
+double ellpk(double m1);
+/**
+ * @brief Incomplete elliptic integral of the first kind F(phi|m)
+ *
+ * @param phi amplitude, [-pi/2, pi/2]
+ * @param m modulus m in [0, 1]
+ * @return double F(phi|m).
+ */
 double ellik(double phi, double m);
-/* ellf/ellpe.c */
-double ellpe(double x);
-/* ellf/ellie.c */
+/**
+ * @brief Complete elliptic integral of the second kind
+ *
+ * @param m1 parameter m1 = 1 - m, in [0, 1]
+ * @return double E(m). Returns 1.0 when m1 == 0; returns 0.0 if m1 is outside [0,1]
+ * @note If you have modulus m, call ellpe(1.0 - m)
+ */
+double ellpe(double m1);
+/**
+ * @brief Incomplete elliptic integral of the second kind E(phi|m)
+ *
+ * @param phi amplitude, [-pi/2, pi/2]
+ * @param m modulus m in [0, 1]
+ * @return double E(phi|m)
+ */
 double ellie(double phi, double m);
 
-/* ellf/ellpj.c */
-int ellpj(double u, double m, double *sn, double *cn, double *dn, double *ph);
+/**
+ * @brief Jacobian elliptic functions sn(u|m), cn(u|m), dn(u|m) and amplitude phi
+ *
+ * @param[in] u real argument u
+ * @param[in] m modulus m in [0, 1]
+ * @param[out] sn sn(u|m) in [-1, 1]
+ * @param[out] cn cn(u|m) in [-1, 1]
+ * @param[out] dn dn(u|m) in (0, 1]
+ * @param[out] phi amplitude; if u = ellik(phi, m), then sn = sin(phi), cn = cos(phi)
+ * @return int 0 on success; returns -1 if m is outside [0,1]
+ * @note Quarter period on the real axis is ellpk(1.0 - m)
+ */
+int ellpj(double u, double m, double *sn, double *cn, double *dn, double *phi);
 
 #if defined(__cplusplus)
 } // extern "C"
