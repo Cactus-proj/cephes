7a917a01364c2a3a1c158a68227cbafa1599d756
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-30 21:44:15 +0800

    docs(prob): add docs

--- include/cephes/prob.h
+++ include/cephes/prob.h
@@ -10,44 +10,60 @@ namespace cephes {
 extern "C" {
 #endif
 
-/* cprob/bdtr.c */
+/* Binomial distribution
+    cprob/bdtr.c */
 double bdtr(int k, int n, double p);
 double bdtrc(int k, int n, double p);
 double bdtri(int k, int n, double y);
 
-/* cprob/btdtr.c */
+/* Beta distribution
+    cprob/btdtr.c */
 double btdtr(double a, double b, double x);
 
-/* cprob/chdtr.c */
+/* Chi-square distribution
+    cprob/chdtr.c */
 double chdtr(double df, double x);
 double chdtrc(double df, double x);
 double chdtri(double df, double y);
 
-/* cprob/fdtr.c */
+/* F distribution
+    cprob/fdtr.c */
 double fdtr(int ia, int ib, double x);
 double fdtrc(int ia, int ib, double x);
 double fdtri(int ia, int ib, double y);
 
-/* cprob/gdtr.c */
+/* Gamma distribution
+    cprob/gdtr.c */
 double gdtr(double a, double b, double x);
 double gdtrc(double a, double b, double x);
 
-/* cprob/nbdtr.c */
+/* Kolmogorov's limiting distribution */
+double kolmogorov(double y);
+double kolmogi(double p);
+
+/* Smirnov distribution */
+double smirnov(int n, double e);
+double smirnovi(int n, double p);
+
+/* Negative binomial distribution
+    cprob/nbdtr.c */
 double nbdtr(int k, int n, double p);
 double nbdtrc(int k, int n, double p);
 double nbdtri(int k, int n, double p);
 
-/* cprob/ndtr.c */
+/* Normal distribution
+    cprob/ndtr.c;ndtri.c  */
 double ndtr(double a);
-/* cprob/ndtri.c */
 double ndtri(double y0_);
 
-/* cprob/pdtr.c */
+/* Poisson distribution
+    cprob/pdtr.c */
 double pdtr(int k, double m);
 double pdtrc(int k, double m);
 double pdtri(int k, double y);
 
-/* cprob/stdtr.c */
+/* Student's t distribution
+    cprob/stdtr.c */
 double stdtr(int k, double t);
 double stdtri(int k, double p);
 
