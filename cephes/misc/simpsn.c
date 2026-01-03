dd0def3deda832af2e212078ac48c1fcac273a57
Chengyu HAN <cyhan.dev@outlook.com>
2026-01-03 17:09:10 +0800

    Add misc.tgz
    Name: misc.tgz
    Size: 55930 bytes : 54 KiB
    CRC32: 5456D319
    CRC64: 862EA0F0A70F4011
    SHA256: 4afe0c1873283a03de023f2f2348a15d24b177e478ddab7e56372ad24b4e3062
    SHA1: 71ce53f3e1a3d4a9037c696051e86657b1dbbb98
    BLAKE2sp: d6a52cb50fcfed9b4790e4131a14a2f7566a19ad640d736a89c2762fdc7cfb04
    MD5: 197a3f565598baa7d7c183c0c4b1963c
    XXH64: 0AE75AE15FDA4E99
    SHA384: cde33d84ec18abc6c6b1f8c3ec72bfbade5f7a9bf1df267c2a715064ada11506ce86ba3efc6280a4edb43a29dd6b5f69
    SHA512: 80e8b2aba2ba4105fa880e0dbbce701c4675c601427f801f6957f3a039e3b834e4e0e524e8c68bc0b387cf1f3d8c9d96be90a5e39496a8ce3e8be37eddbe51ec
    SHA3-256: 45458f293b2ba6d7fd4b790d75c890d3f1c393af58e0c24832de620f72c959ee

--- cephes/misc/simpsn.c
+++ cephes/misc/simpsn.c
@@ -22,9 +22,13 @@ static double simcon[] =
 */
 
 /* 8th order formula */
-static double simcon[] = {
-    3.488536155202821869E-2,  2.076895943562610229E-1,   -3.27336860670194003527E-2,
-    3.7022927689594356261E-1, -1.6014109347442680776E-1,
+static double simcon[] =
+{
+  3.488536155202821869E-2,
+  2.076895943562610229E-1,
+ -3.27336860670194003527E-2,
+  3.7022927689594356261E-1,
+ -1.6014109347442680776E-1,
 };
 
 /* 10th order formula */
@@ -39,7 +43,7 @@ static double simcon[] =
   7.1376463043129709796E-1,
 };
 */
-
+
 /*							simpsn.c 2	*/
 /* 20th order formula */
 /*
@@ -58,19 +62,20 @@ static double simcon[] =
  -9.0005367135242894657916E+1,
 };
 */
-
+
 /*							simpsn.c 3	*/
-double simpsn(f, delta)
-double f[];   /* tabulated function */
-double delta; /* spacing of arguments */
+double simpsn( f, delta )
+double f[];	/* tabulated function */
+double delta;	/* spacing of arguments */
 {
-    extern double simcon[];
-    double ans;
-    int i;
+extern double simcon[];
+double ans;
+int i;
 
-    ans = simcon[NCOTE / 2] * f[NCOTE / 2];
-    for (i = 0; i < NCOTE / 2; i++)
-        ans += simcon[i] * (f[i] + f[NCOTE - i]);
 
-    return (ans * delta * NCOTE);
+ans = simcon[NCOTE/2] * f[NCOTE/2];
+for( i=0; i < NCOTE/2; i++ )
+	ans += simcon[i] * ( f[i] + f[NCOTE-i] );
+
+return( ans * delta * NCOTE );
 }
